bc450ba41c2729745bf122b94f6e7a8603e870fa
Sean <sean.s.anastasi@gmail.com>
2021-02-06 00:10:27 +0100

    Add basic buttons to chat area

--- mobileapp/src/components/Chat.js
+++ mobileapp/src/components/Chat.js
@@ -1,13 +1,17 @@
+import { Button } from '@material-ui/core';
 import React from 'react';
-
+import { useHistory } from 'react-router';
 
 
 function Chat (){
     
+    const history = useHistory();
     
-
 return(
-    <div>
+    <div className="chat-buttons">
+        
+            <Button className="chat-button" variant="contained" color="primary" >Chat with a therapist</Button>
+            <Button variant="contained" color="primary" >Chat with a random person</Button>
         
     </div>
 );
