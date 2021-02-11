fb6237934c8003e2d9ecd68ec658841ce830554d
Sean <sean.s.anastasi@gmail.com>
2021-02-11 18:09:35 +0100

    Like system done

--- mobileapp/src/components/SingleForum.js
+++ mobileapp/src/components/SingleForum.js
@@ -1,15 +1,23 @@
 import React, {useState, useEffect} from "react";
-import Icon from '@material-ui/core/Icon';
 import FavoriteBorderOutlinedIcon from '@material-ui/icons/FavoriteBorderOutlined';
 import FavoriteOutlinedIcon from '@material-ui/icons/FavoriteOutlined';
-import { firestore } from "../firebase";
+import firebase from "firebase";
+import { firebaseConfig, firestore } from "../firebase.js";
 
 export default function SingleForum(props){
     const id = props.match.params.id;
     const[forum, setForum] = useState([]);
-    var forumNumericalId = -1;
+    const [forumNumericalId, setForumNumericalId ] = useState(-1);
     const[iconFilled, setIconFilled] = useState(false);
-    useEffect( () => {
+    const[likes, setLikes] = useState([]);
+    const user = JSON.parse(
+        window.sessionStorage.getItem(
+            `firebase:authUser:${firebaseConfig.apiKey}:[DEFAULT]`
+        )
+    );
+
+    useEffect(() => {
+        
          firestore
         .collection("Forums")
         .get()
@@ -25,22 +33,89 @@ export default function SingleForum(props){
             
         });
         
+    
+        
+        
     }, []);
+    useEffect(() => { 
     if(forum != null){
+        console.log("IN EFFECT");
         for(var i = 0; i< forum.length; i++){
-            if(forum[i].ForumId == id){
-                forumNumericalId = i;
+            if(forum[i].ForumId === id){
+                setForumNumericalId(i);
                 
             }
         }
     }
+    }, [forum]);
+
+    useEffect(() => { 
+          
+            firestore
+            .collection("Likes")
+            .get()
+            .then(querySnapshot => {
+                const _likes = querySnapshot.docs.map(doc => {
+                    let d = doc.data();
+                    
+                    return d;               
+                    
+                });
+                
+                setLikes(_likes);
+                
+                
+            });         
+        
+
+        
+        
+
+        
+        
+    }, []);
+
+    useEffect(() => { 
+    if(user && likes.length > 0){
+        console.log(likes);
+        for(var i = 0; i< likes.length; i++){
+            if(likes[i].ForumId == id && likes[i].UserId == user.uid){
+                    setIconFilled(true);
+            }
+        }
+    } 
+    }, [likes]);
     
     const favouriteClick = () => {
-        setIconFilled(!iconFilled);
+        if(user){
 
-        console.log(props);
+        
+        if(!iconFilled){
+            
+            firestore.collection("Likes").add({
+                ForumId: id,
+                UserId: user.uid
+            })
+            setLikes(likes,{
+                ForumId: id,
+                UserId: user.uid
+            });
+            
+        }else{
+            
+            firestore.collection("Likes").where("ForumId","==", id).where("UserId", "==", user.uid).get()
+            .then(querySnapshot => {
+                querySnapshot.docs[0].ref.delete();
+            });
+            
+        }
+       setIconFilled(!iconFilled);
+        // setLikes(likes);
+        console.log(iconFilled);
+        
     }
-
+    }
+    
     if(forum[forumNumericalId]){
 
         
