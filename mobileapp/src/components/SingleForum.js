dc5ca94bfbdefa3d6342b1aa05e42b2ea4a1cf4a
Sean <sean.s.anastasi@gmail.com>
2021-02-14 00:58:39 +0100

    Prompt login on like/comment when not logged in

--- mobileapp/src/components/SingleForum.js
+++ mobileapp/src/components/SingleForum.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from "react";
+import React, {useState, useEffect, useContext} from "react";
 import FavoriteBorderOutlinedIcon from '@material-ui/icons/FavoriteBorderOutlined';
 import FavoriteOutlinedIcon from '@material-ui/icons/FavoriteOutlined';
 import firebase from "firebase";
@@ -6,10 +6,98 @@ import { firebaseConfig, firestore } from "../firebase.js";
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
-import { ListSubheader } from "@material-ui/core";
 import Divider from '@material-ui/core/Divider';
+import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core/styles';
+import Avatar from '@material-ui/core/Avatar';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import Dialog from '@material-ui/core/Dialog';
+import PersonIcon from '@material-ui/icons/Person';
+import AddIcon from '@material-ui/icons/Add';
+import { blue } from '@material-ui/core/colors';
+import {AuthContext} from "../index.js";
+import { toast } from "react-toastify";
+import Button from "@material-ui/core/Button";
+
+const emails = ['username@gmail.com', 'user02@gmail.com'];
+const useStyles = makeStyles({
+  avatar: {
+    backgroundColor: blue[100],
+    color: blue[600],
+  },
+});
+
+function SimpleDialog(props) {
+  const classes = useStyles();
+  const { onClose, selectedValue, open } = props;
+  const Auth = useContext(AuthContext);
+  var temp;
+  const handleClose = () => {
+    onClose(selectedValue);
+  };
+  const handleGoogleAuth = () => {
+    if(!Auth.isLoggedIn) {
+        const provider = new firebase.auth.GoogleAuthProvider();
+        firebase.auth().setPersistence(firebase.auth.Auth.Persistence.SESSION)
+        .then(() => {
+            firebase.auth().signInWithPopup(provider).then(async (result) => {
+                temp = await firestore.collection("users").doc(result.user.email).get()
+                if(!(temp.exists)) {
+                    console.log("Not Exists")
+                    await firestore.collection("users").doc(result.user.email).set({
+                        email: result.user.email,
+                        displayName: result.user.displayName
+                    }).then(() => {
+                        //props.history.push("/users")
+                        toast.success("Successfully added new account")
+                    })
+                } else {
+                    console.log("Exists", temp.data())
+                }
+                console.log("RESULT", result);
+                Auth.setLoggedIn(true);
+                handleClose();
+            }).catch((e) => console.error(e));
+            
+        });
+
+        
+    }
+    else {
+        firebase.auth().signOut().then(function() {
+            Auth.setLoggedIn(false);
+        })
+    }
+};
+
+  return (
+    <Dialog onClose={handleClose} aria-labelledby="simple-dialog-title" open={open}>
+      <DialogTitle id="simple-dialog-title">Please Login to Continue</DialogTitle>
+      <Button className="accessibleButton" variant="contained" style={{color: "white", textDecoration:"none", fontSize: "11px"}} onClick={handleGoogleAuth}>
+                    Sign in
+        </Button>
+    </Dialog>
+  );
+}
+
+SimpleDialog.propTypes = {
+  onClose: PropTypes.func.isRequired,
+  open: PropTypes.bool.isRequired,
+  selectedValue: PropTypes.string.isRequired,
+};
+
 
 export default function SingleForum(props){
+    const [open, setOpen] = React.useState(false);
+  const [selectedValue, setSelectedValue] = React.useState(emails[1]);
+
+ 
+
+  const handleClose = (value) => {
+    setOpen(false);
+    setSelectedValue(value);
+  }
     const id = props.match.params.id;
     const[forum, setForum] = useState([]);
     const[comments, setComments] = useState([]);
@@ -22,6 +110,7 @@ export default function SingleForum(props){
             `firebase:authUser:${firebaseConfig.apiKey}:[DEFAULT]`
         )
     );
+    
 
     useEffect(() => {
         
@@ -131,8 +220,10 @@ export default function SingleForum(props){
                 UserId: user.uid,
                 Username: user.displayName
             }]);
-            console.log(comments);
+           
             
+        }else if (!user){
+            setOpen(true);
         }
         setPlaceholder("");
     }
@@ -169,6 +260,8 @@ export default function SingleForum(props){
         // setLikes(likes);
         
         
+    }else{
+        setOpen(true);
     }
     }
     
@@ -203,6 +296,7 @@ export default function SingleForum(props){
                     
                 </div>
             </form>
+            <SimpleDialog selectedValue={selectedValue} open={open} onClose={handleClose} />
         </div>
         );
     }
